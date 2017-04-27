d3ed8621f061f228ec01ee25678fb19a637a71b3
= <bonebare@gmail.com>
2017-04-27 14:36:04 +0100

    advanced webdriver 50% completed

--- WDriverAdvanced/WDriverAdvanced/Program.cs
+++ WDriverAdvanced/WDriverAdvanced/Program.cs
@@ -6,7 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+using OpenQA.Selenium.Support.UI;
 
 namespace WDriverAdvanced
 {
@@ -14,14 +17,42 @@ namespace WDriverAdvanced
     {
         static void Main(string[] args)
         {
-            IWebDriver myDriver = new FirefoxDriver();
-            myDriver.Url = "file:///C:/Users/ddsilva/OneDrive/Selenium/Webdriver/WDriverAdvanced/WDriverAdvanced/TestPage.html";
+            IWebDriver myDriver = new ChromeDriver();
+            myDriver.Url = @"D:\My Files\OneDrive\Selenium\Webdriver\WDriverAdvanced\WDriverAdvanced\TestPage.html";
 
-            var radiodButton = myDriver.FindElements(By.Name("color"))[1];
-            radiodButton.Click();
+            // RADIOBUTTONS
+            //var radioButtons = myDriver.FindElements(By.Name("color"));
+
+            //foreach (var radioButton in radioButtons)
+            //{
+            //    if(radioButton.Selected)
+            //        Console.WriteLine(radioButton.GetAttribute("value"));
+            //}
+
+            // CHECKBOX
+            //var checkBox = myDriver.FindElement(By.Id("check1"));
+            //checkBox.Click();
+            //Thread.Sleep(2000);
+            //checkBox.Click();
+
+
+            // SELECT OPTIONS
+            var select = myDriver.FindElement(By.Id("select1"));
+
+            // General
+            //var aOption = select.FindElements(By.TagName("option"))[3];
+            //aOption.Click();
+
+            // Using SelectElement
+            var selectElement = new SelectElement(select);
+            selectElement.SelectByText("Joe");
+
+
+
+
+
 
             // myDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
             // myDriver.FindElement(By.ClassName("gb_7c")).Click();
         }
     }
